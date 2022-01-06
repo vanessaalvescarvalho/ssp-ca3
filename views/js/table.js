505d7a16b5d9c0651d0765785776392ad3a1f4a9
vanessaalvescarvalho <vanealvescarvalho@gmail.com>
2022-01-06 15:11:04 +0000

    create table.js file

---
+++ views/js/table.js
@@ -0,0 +1,24 @@
+function draw_table()
+{
+	$("#results").empty();
+	$.getJSONuncached = function (url)
+	{
+		return $.ajax(
+		{
+			url: url,
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#results").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/get/html")
+};
+
+$(document).ready(function ()
+{
+	draw_table();
+});
